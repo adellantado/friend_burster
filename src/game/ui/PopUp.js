679af7730eb22496b80846744ac92562c8de357f
maxpaint <lovemaxpaint@gmail.com>
2014-11-08 03:28:12 +0200

    added PopUp.js

---
+++ src/game/ui/PopUp.js
@@ -0,0 +1,47 @@
+/**
+ * Created by maxpaint on 08.11.2014.
+ */
+
+PopupManager = (function(){
+
+    var _dialogUI =  $( "#dialog" );
+
+
+    function showAlert(message, title, object){
+        console.log("DATA", _dialogUI.attr("title"));
+        _dialogUI.empty();
+        console.log("DATA", _dialogUI.attr("title"));
+        _dialogUI.attr( "title", title );
+        _dialogUI.append( "<div>"+ message +"</div>" );
+
+        if(object == null){
+            _dialogUI.dialog({
+                    resizable: false,
+                    height:140,
+                    modal: true
+                }
+            );
+        }else{
+            _dialogUI.dialog(object);
+        }
+    }
+
+    return {
+        showAlert: function(message, title){
+            showAlert(message, title);
+        }
+    }
+})();
+
+
+$( "#dialog-confirm" ).dialog({
+
+    buttons: {
+        "Delete all items": function() {
+            $( this ).dialog( "close" );
+        },
+        Cancel: function() {
+            $( this ).dialog( "close" );
+        }
+    }
+});
