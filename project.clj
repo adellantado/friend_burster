0754e565e27ea83beaba0f82f158cbb89f9edd37
Andrii Zhukov <andrii.zhukov@db.com>
2014-11-28 19:01:01 +0200

    some config changes

--- project.clj
+++ project.clj
@@ -5,18 +5,18 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2202"]
+                 [org.clojure/clojurescript "0.0-2371"]
                  [com.facebook/react "0.11.1"]
                  [figwheel "0.1.4-SNAPSHOT"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [sablono "0.2.21"]
-                 [om "0.7.1"]]
-  
+                 [om "0.8.0-alpha2"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-figwheel "0.1.4-SNAPSHOT"]]
 
   :source-paths ["src"]
-  
+
   :cljsbuild {
     :builds [{:id "dev"
               :source-paths ["src"]
@@ -31,11 +31,11 @@
                          :pretty-print false
                          :preamble ["react/react.min.js"]
                          :externs [
-                                   ;;"src/react/externs/react.js" 
-                                   "src/tweenjs/Tween.js" 
+                                   ;;"src/react/externs/react.js"
+                                   "src/tweenjs/Tween.js"
                                    "src/tweenjs/Ease.js"]}}]}
   :figwheel {
-             :http-server-root "public" ;; default and assumes "resources" 
+             :http-server-root "public" ;; default and assumes "resources"
              :server-port 3449 ;; default
              :css-dirs ["public/resources/css"] ;; watch and update CSS
              ;; :ring-handler friend_burster.server/handler
--- src/friend_burster/soc_client.cljs
+++ src/friend_burster/soc_client.cljs
@@ -1,10 +1,82 @@
 (ns friend_burster.soc_client
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require
-   [figwheel.client :as fw :include-macros true]))
+   [figwheel.client :as fw :include-macros true]
+   [goog.dom :as dom]
+   [goog.events :as events]
+   [cljs.core.async :as async :refer [>! <! put! chan alts!]])
+  (:import [goog.events EventType])
+  )
 
 (enable-console-print!)
 
-(defn start [] (print "start!"))
+
+ ;;(print async/version)
+
+(defn events->chan
+  "Given a target DOM element and event type return a channel of
+  observed events. Can supply the channel to receive events as third
+  optional argument."
+  ([el event-type] (events->chan el event-type (chan)))
+  ([el event-type c]
+     (events/listen el event-type
+                    (fn [e] (put! c e)))
+     c))
+
+(defn mouse-loc->vec
+  "Given a Google Closure normalized DOM mouse event return the
+  mouse x and y position as a two element vector."
+  [e]
+    [(.-clientX e) (.-clientY e)])
+
+
+(defn ex6 []
+  (let [button (dom/getElement "canvas")
+        clicks (events->chan button EventType.CLICK)
+        mouse  (events->chan js/window EventType.MOUSEMOVE
+                             (chan 1 (map mouse-loc->vec))
+                             ;(chan)
+                             )]
+    (go
+      (print "Click button to start tracking the mouse!")
+      (<! clicks)
+      (print "clicked")
+      (loop []
+        (let [[v c] (alts! [mouse clicks])]
+          (cond
+           (= c clicks) (print "Done!")
+           :else
+           (do
+             (print (pr-str v))
+                           (recur))))))))
+
+(ex6)
+
+
+#_(go
+  (while true
+    (alts! [(chan) (chan)])))
+
+#_(let [a (chan)
+      b (chan)]
+  (go
+    (loop []
+      (print (alts! [a b])))))
+
+
+(defn listen [el type]
+  (let [out (chan)]
+    (events/listen el type
+                   (fn [e] (put! out e)))
+    out))
+
+
+
+
+
+(defn start [] (do (print "start!")
+                   ;;(init-stuff)
+                   ))
 
 (def xxxx "my string")
 
@@ -25,6 +97,26 @@
   )
 
 
+(defn draw-circle [ctx x y r col]
+  (doto ctx
+    .beginPath
+    (.arc x y r 0 (* 2 PI) false)
+    (aset "fillStyle" col)
+    .fill))
+
+
+(defn draw-frame [cnv ctx]
+  (doto ctx
+    .beginPath
+    ;(aset  "lineWidth" "1")
+    (.rect 0.5 0.5 (- (.-width cnv) 1) (- (.-height cnv) 1))
+    .stroke))
+
+(defn draw-frame-util []
+  (let [cnv (dom/getElement "canvas")
+        ctx (.getContext cnv "2d")]
+    (draw-frame cnv ctx)))
+
 
 (defn render []
   (let [canvas (.getElementById js/document "canvas")
@@ -33,17 +125,10 @@
     (doseq [[_ {x :x y :y col :color}] @state]
       (draw-circle ctx x y 10 col))))
 
-
-
-
-(defn create-ws []
-  (let [ws (js/WebSocket.
-             "ws://10.249.134.74:8080/examples/websocket/echoProgrammatic")]
-    (doto ws
-        (aset "onerror" (fn [error] (print "some error" (.-data error))))
-        (aset "onopen" (fn [evt] (print "connection OK")))
-        (aset "onmessage" on-message)
-        (aset "onclose" (fn [evt] (print "Socket has been closed: " (.-data evt)))))))
+(defn clear []
+  (let [canvas (.getElementById js/document "canvas")
+        ctx (.getContext canvas "2d")]
+    (.clearRect ctx 0 0 (.-width canvas) (.-height canvas))))
 
 (defn rnd [max]
   (* max (.random js/Math)))
@@ -51,7 +136,7 @@
 
 (defn on-message [evt]
   (let [
-        ;; data (.-data evt)
+        data (.-data evt)
         command "move"]
 ;    (print data)
     (case command
@@ -69,18 +154,61 @@
                    new-col (str "rgb(" (rnd 255) "," (rnd 255) "," (rnd 255) ")")
                    ;list (map (fn [item] ()) data-list)
                    ]
-               (spown-user ))
+               (spawn-user ))
       (print "unknown command:" command))))
 
 
-(defn draw-circle [ctx x y r col]
-  (doto ctx
-    .beginPath
-    (.arc x y r 0 (* 2 PI) false)
-    (aset "fillStyle" col)
-    .fill))
+(defn create-ws []
+  (let [ws (js/WebSocket.
+             "ws://10.249.134.74:8080/examples/websocket/echoProgrammatic")]
+    (doto ws
+        (aset "onerror" (fn [error] (print "some error" (.-data error))))
+        (aset "onopen" (fn [evt] (print "connection OK")))
+        (aset "onmessage" on-message)
+        (aset "onclose" (fn [evt] (print "Socket has been closed: " (.-data evt)))))))
 
 
+
+
+
+
+
+
+
+
+;;(clear)
+;;(draw-frame-util)
+
+
+(defn init-stuff []
+  (let [canvas (dom/getElement "canvas")
+        down (listen canvas "mousedown")
+        up (listen canvas "mouseup")
+        move (listen canvas "mousemove")]
+
+    #_(go
+      (loop []
+        (let [ch (<! up)]
+          (do
+            (.log js/console val "zzsssdd") (recur)))))
+
+    #_(go (loop []
+          (let [[val ch] (alt! [down up move])]
+            (condp = ch
+              down (do (.log js/console "downnn") (recur))
+              up (do (.log js/console "downnn") (recur))
+              move (do (.log js/console "downnn") (recur))
+              (recur)))))
+
+
+    #_(go
+      (while true
+        (.log js/console
+              (alts! [up down]) "cooll")))))
+
+(init-stuff)
+
+;;-----------------------------------------------
 (fw/watch-and-reload
   :websocket-url   "ws://localhost:3449/figwheel-ws"
   :jsload-callback (fn [] (print "reloaded")))
